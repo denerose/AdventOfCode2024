28b89b5dd1c99f6d2d48e427b99b6406cbd97d76
Saphron H <109723086+denerose@users.noreply.github.com>
2024-12-03 09:19:40 +1100

    Old Attempt Day 2 (Part 2)

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -2,6 +2,7 @@ package Day2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day2 {
@@ -13,17 +14,19 @@ public class Day2 {
         try {
             File inputFile = new File("Day2/input.txt");
             Scanner input = new Scanner(inputFile);
+
             while (input.hasNextLine()) {
                 String line = input.nextLine();
                 String[] values = line.split(" ");
-                int[] intValues = new int[values.length];
+                ArrayList<Integer> intValues = new ArrayList<>();
                 for (int i = 0; i < values.length; i++) {
-                    intValues[i] = Integer.parseInt(values[i]);
+                    intValues.add(Integer.parseInt(values[i]));
                 }
-                if (isSafe(intValues)) {
+                if (isSafeWithSave(intValues, true)) {
                     countSafe++;
                 }
             }
+
             input.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
@@ -33,7 +36,82 @@ public class Day2 {
         System.out.println("Safe Count: " + countSafe);
     }
 
-    static boolean isSafe(int[] values) {
+    static boolean isSafeWithSave(ArrayList<Integer> values, boolean saveAvailable) {
+
+        boolean increasing = false;
+        boolean decreasing = false;
+
+        int errorCount = 0;
+        int errorIndex = -1;
+
+        for (int i = 0; i < values.size() - 1; i++) {
+
+            int diff = Math.abs(values.get(i) - values.get(i + 1));
+            if (diff > 3 || diff < 1) {
+                if (saveAvailable) {
+                    errorCount++;
+                    saveAvailable = false;
+                    if (i != 0) {
+                        int newDiff = Math.abs(values.get(i - 1) - values.get(i + 1));
+                        if (newDiff <= 3 && newDiff >= 1) {
+                            values.remove(i);
+                            return isSafeWithSave(values, false);
+                        }
+                    }
+                    if (values.size() > i + 2) {
+                        int newDiff = Math.abs(values.get(i) - values.get(i + 2));
+                        if (newDiff <= 3 && newDiff >= 1) {
+                            values.remove(i + 1);
+                            return isSafeWithSave(values, false);
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            }
+
+            if (values.get(i) < values.get(i + 1)) {
+                increasing = true;
+                if (decreasing) {
+                    if (saveAvailable) {
+                        errorCount++;
+                        saveAvailable = false;
+                        if ((values.get(i - 1) < values.get(i + 1))) {
+                            values.remove(i);
+                            return isSafeWithSave(values, false);
+                        } else {
+                            values.remove(i - 1);
+                            return isSafeWithSave(values, false);
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+            } else if (values.get(i) > values.get(i + 1)) {
+                if (increasing) {
+                    if (saveAvailable) {
+                        errorCount++;
+                        saveAvailable = false;
+                        if ((values.get(i - 1) < values.get(i + 1))) {
+                            values.remove(i);
+                            return isSafeWithSave(values, false);
+                        } else {
+                            values.remove(i - 1);
+                            return isSafeWithSave(values, false);
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                decreasing = true;
+            }
+        }
+
+        return ((increasing && !decreasing) || (!increasing && decreasing));
+
+    }
+
+    static boolean isSafeTask1(int[] values) {
 
         boolean increasing = false;
         boolean decreasing = false;
