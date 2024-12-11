4a18043c214592481e5387731fdfa38e1ab1f692
Saphron H <109723086+denerose@users.noreply.github.com>
2024-12-11 18:26:38 +1100

    Split-em up methods

--- Day11/Day11.java
+++ Day11/Day11.java
@@ -1,35 +1,49 @@
 package Day11;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.io.File;
-import java.lang.ref.SoftReference;
 
 public class Day11 {
     public static void main(String[] args) {
         System.out.println("Hello, Advent of Code 2024: Day 10");
 
-        ArrayList<Long> stones = new ArrayList<>();
+        ArrayList<Long> stones = new ArrayList<>(104739000);
         readInput(stones);
 
+        int totalIterations = 0;
+        int stoneCount = 0;
+
         // Part 1
-        // ArrayList<Long> part1 = blink(stones, 25);
-        // System.out.println("Part 1: " + part1.size());
+        ArrayList<Long> part1 = blink(stones, 25);
+        totalIterations += 25;
+        stoneCount = part1.size();
+        System.out.println("Part 1: " + part1.size());
 
         // Part 2
-        ArrayList<Long> part2 = blink(stones, 75);
-        System.out.println("Part 2: " + part2.size());
+        part1 = blinkBetter(part1, 10);
+        totalIterations += 10;
+        stoneCount = part1.size();
+
+        List<Long> left = blinkBetter(part1.subList(0, part1.size() / 2), 10);
+        List<Long> right = blinkBetter(part1.subList(part1.size() / 2, part1.size()), 10);
+
+        totalIterations += 10;
+        stoneCount = left.size() + right.size();
+
+        System.out.println("Part 2: " + stoneCount + "\n Total Iterations: " + totalIterations);
 
     }
 
     static ArrayList<Long> blink(ArrayList<Long> stones, int counter) {
 
-        ArrayList<Long> newStones = new ArrayList<>();
+        ArrayList<Long> newStones = new ArrayList<>(stones.size() * 2);
 
         for (long stone : stones) {
             if (stone == 0) {
                 newStones.add(1l);
-            } else if (stone != 0 && (Math.floor(Math.log10(stone)) + 1) % 2 == 0) {
+            } else if ((Math.floor(Math.log10(stone)) + 1) % 2 == 0) {
                 String numString = Long.toString(stone);
                 int middle = numString.length() / 2;
                 long left = Long.parseLong(numString.substring(0, middle));
@@ -50,14 +64,14 @@ public class Day11 {
 
     }
 
-    static ArrayList<Long> blinkBetter(ArrayList<Long> stones, int counter) {
+    static ArrayList<Long> blinkBetter(List<Long> stones, int counter) {
 
-        ArrayList<Long> newStones = new ArrayList<>();
+        ArrayList<Long> newStones = new ArrayList<>(stones.size() * 2);
 
         for (long stone : stones) {
             if (stone == 0) {
                 newStones.add(1l);
-            } else if (stone != 0 && (Math.floor(Math.log10(stone)) + 1) % 2 == 0) {
+            } else if (Math.floor(Math.log10(stone) + 1) % 2 == 0) {
                 String numString = Long.toString(stone);
                 int middle = numString.length() / 2;
                 long left = Long.parseLong(numString.substring(0, middle));
@@ -74,7 +88,7 @@ public class Day11 {
         } else {
             System.out.println("Counter: " + counter + ". Stones: " + newStones.size());
             stones = newStones;
-            return blink(stones, counter - 1);
+            return blinkBetter(stones, counter - 1);
         }
 
     }
