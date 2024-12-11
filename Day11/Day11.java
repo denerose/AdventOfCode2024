9a5782a7bf15efe9866d8b3e955a9c91fb13a731
Saphron H <109723086+denerose@users.noreply.github.com>
2024-12-12 09:37:34 +1100

    Complete Day 11 (Part 2)

--- Day11/Day11.java
+++ Day11/Day11.java
@@ -1,9 +1,11 @@
 package Day11;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.io.File;
+import java.lang.reflect.Array;
 
 public class Day11 {
     public static void main(String[] args) {
@@ -12,27 +14,29 @@ public class Day11 {
         ArrayList<Long> stones = new ArrayList<>(104739000);
         readInput(stones);
 
-        int totalIterations = 0;
-        int stoneCount = 0;
-
         // Part 1
-        ArrayList<Long> part1 = blink(stones, 25);
-        totalIterations += 25;
-        stoneCount = part1.size();
-        System.out.println("Part 1: " + part1.size());
+        stones = blink(stones, 25);
+        System.out.println("Part 1: " + stones.size());
 
         // Part 2
-        part1 = blinkBetter(part1, 10);
-        totalIterations += 10;
-        stoneCount = part1.size();
+        HashMap<Long, Long> stoneMap = mapStones(stones);
+        HashMap<Long, long[]> calculations = new HashMap<>();
+        calculations.put(0l, new long[] { 1 });
+        calculations.put(1l, new long[] { 2024 });
+        calculations.put(2024l, new long[] { 20, 24 });
+        calculations.put(20l, new long[] { 2, 0 });
+        calculations.put(24l, new long[] { 2, 4 });
+        calculations.put(2l, new long[] { 4048 });
+        calculations.put(4l, new long[] { 8096 });
+        calculations.put(4048l, new long[] { 40, 48 });
+        calculations.put(8096l, new long[] { 80, 96 });
+        calculations.put(40l, new long[] { 4, 0 });
+        calculations.put(48l, new long[] { 4, 8 });
+        calculations.put(80l, new long[] { 8, 0 });
+        calculations.put(96l, new long[] { 9, 6 });
 
-        List<Long> left = blinkBetter(part1.subList(0, part1.size() / 2), 10);
-        List<Long> right = blinkBetter(part1.subList(part1.size() / 2, part1.size()), 10);
-
-        totalIterations += 10;
-        stoneCount = left.size() + right.size();
-
-        System.out.println("Part 2: " + stoneCount + "\n Total Iterations: " + totalIterations);
+        stoneMap = blinkBetter(stoneMap, 50, calculations);
+        System.out.println("Part 2: " + stoneMap.values().stream().mapToLong(Long::longValue).sum());
 
     }
 
@@ -64,31 +68,67 @@ public class Day11 {
 
     }
 
-    static ArrayList<Long> blinkBetter(List<Long> stones, int counter) {
-
-        ArrayList<Long> newStones = new ArrayList<>(stones.size() * 2);
-
+    static HashMap<Long, Long> mapStones(ArrayList<Long> stones) {
+        HashMap<Long, Long> stoneMap = new HashMap<>(stones.size());
         for (long stone : stones) {
-            if (stone == 0) {
-                newStones.add(1l);
-            } else if (Math.floor(Math.log10(stone) + 1) % 2 == 0) {
+            if (stoneMap.containsKey(stone)) {
+                stoneMap.put(stone, stoneMap.get(stone) + 1);
+            } else {
+                stoneMap.put(stone, 1l);
+            }
+        }
+        return stoneMap;
+    }
+
+    static HashMap<Long, Long> blinkBetter(HashMap<Long, Long> stones, int counter,
+            HashMap<Long, long[]> calculations) {
+
+        HashMap<Long, Long> newStones = new HashMap<>();
+
+        stones.entrySet().stream().filter(entry -> calculations.containsKey(entry.getKey())).forEach(entry -> {
+            long stone = entry.getKey();
+            long count = entry.getValue();
+            long[] values = calculations.get(stone);
+            for (long value : values) {
+                if (newStones.containsKey(value)) {
+                    newStones.put(value, newStones.get(value) + count);
+                } else {
+                    newStones.put(value, count);
+                }
+            }
+        });
+
+        stones.entrySet().stream().filter(entry -> !calculations.containsKey(entry.getKey())).forEach(entry -> {
+            long stone = entry.getKey();
+            long count = entry.getValue();
+            if ((Math.floor(Math.log10(stone)) + 1) % 2 == 0) {
                 String numString = Long.toString(stone);
                 int middle = numString.length() / 2;
                 long left = Long.parseLong(numString.substring(0, middle));
                 long right = Long.parseLong(numString.substring(middle));
-                newStones.add(left);
-                newStones.add(right);
+                calculations.put(stone, new long[] { left, right });
+                if (newStones.containsKey(left)) {
+                    newStones.put(left, newStones.get(left) + count);
+                } else {
+                    newStones.put(left, count);
+                }
+                if (newStones.containsKey(right)) {
+                    newStones.put(right, newStones.get(right) + count);
+                } else {
+                    newStones.put(right, count);
+                }
             } else {
-                newStones.add(stone * 2024);
+                calculations.put(stone, new long[] { stone * 2024 });
+                newStones.put(stone * 2024, count);
             }
-        }
+        });
+
         if (counter == 1) {
-            System.out.println("Counter: " + counter + ". Stones: " + newStones.size());
+            System.out.println("Counter: " + counter + ". Nums: " + newStones.size());
             return newStones;
         } else {
-            System.out.println("Counter: " + counter + ". Stones: " + newStones.size());
-            stones = newStones;
-            return blinkBetter(stones, counter - 1);
+            System.out.println("Counter: " + counter + ". Nums: " + newStones.size());
+            return blinkBetter(newStones, counter - 1, calculations);
         }
 
     }
