18c61ca207169e9af7527f2806025b52157c6fbd
Saphron H <109723086+denerose@users.noreply.github.com>
2024-12-11 16:45:16 +1100

    Prep Day 12-14

---
+++ Day12/Day12.java
@@ -0,0 +1,26 @@
+package Day12;
+
+import java.util.Scanner;
+import java.io.File;
+
+public class Day12 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Advent of Code 2024: Day 10");
+    }
+
+    // Read input from file
+    public static void readInput() {
+        try (Scanner scanner = new Scanner(new File("Day10/input.txt"))) {
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error reading input");
+            e.printStackTrace();
+        }
+    }
+
+}
