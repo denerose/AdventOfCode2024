b8e479d0217fbded4caa2086729651970a74186b
Saphron H <109723086+denerose@users.noreply.github.com>
2024-12-11 17:55:26 +1100

    Setup Day 11 Part 2

--- Day11/Day11.java
+++ Day11/Day11.java
@@ -3,6 +3,7 @@ package Day11;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
+import java.lang.ref.SoftReference;
 
 public class Day11 {
     public static void main(String[] args) {
@@ -12,9 +13,12 @@ public class Day11 {
         readInput(stones);
 
         // Part 1
-        System.out.println("Stones: " + stones);
-        ArrayList<Long> part1 = blink(stones, 25);
-        System.out.println("Part 1: " + part1.size());
+        // ArrayList<Long> part1 = blink(stones, 25);
+        // System.out.println("Part 1: " + part1.size());
+
+        // Part 2
+        ArrayList<Long> part2 = blink(stones, 75);
+        System.out.println("Part 2: " + part2.size());
 
     }
 
@@ -37,9 +41,40 @@ public class Day11 {
             }
         }
         if (counter == 1) {
+            System.out.println("Counter: " + counter + ". Stones: " + newStones.size());
             return newStones;
         } else {
+            System.out.println("Counter: " + counter + ". Stones: " + newStones.size());
             return blink(newStones, counter - 1);
+        }
+
+    }
+
+    static ArrayList<Long> blinkBetter(ArrayList<Long> stones, int counter) {
+
+        ArrayList<Long> newStones = new ArrayList<>();
+
+        for (long stone : stones) {
+            if (stone == 0) {
+                newStones.add(1l);
+            } else if (stone != 0 && (Math.floor(Math.log10(stone)) + 1) % 2 == 0) {
+                String numString = Long.toString(stone);
+                int middle = numString.length() / 2;
+                long left = Long.parseLong(numString.substring(0, middle));
+                long right = Long.parseLong(numString.substring(middle));
+                newStones.add(left);
+                newStones.add(right);
+            } else {
+                newStones.add(stone * 2024);
+            }
+        }
+        if (counter == 1) {
+            System.out.println("Counter: " + counter + ". Stones: " + newStones.size());
+            return newStones;
+        } else {
+            System.out.println("Counter: " + counter + ". Stones: " + newStones.size());
+            stones = newStones;
+            return blink(stones, counter - 1);
         }
 
     }
@@ -52,6 +87,7 @@ public class Day11 {
                 stones.add(scanner.nextLong());
             }
 
+            scanner.close();
         } catch (Exception e) {
             System.out.println("Error reading input");
             e.printStackTrace();
